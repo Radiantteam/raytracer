4d9f12aa318f693500e1e53aa7a6de9497fcc326
naxxer <abdoragragy1@gmail.com>
2025-10-31 15:39:32 +0100

    SCRUM-83: Implement cube type and adding an abstract parent class

--- src/sphere/Sphere.hpp
+++ src/sphere/Sphere.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
-#include "Image.hpp"
+#include "../Shape.hpp"
 #include "../vec/Vec3.hpp"
+#include "image/Image.hpp"
 
 // Dessine une sphère centrée (cx, cy), rayon en pixels, teintée par baseColor.
 // Éclairage minimal : ambiant + Lambert
-class Sphere {
+class Sphere : public Shape {
 public:
-    static void Draw(
-        Image& img,
-        int cx, int cy,
-        float radius,
-        const Color& baseColor
-    );
+    Sphere(const Vec3& center, float radius, const Color& color);
+
+    void Draw(Image& img) const override;
+
+private:
+    Vec3 _center;
+    float _radius;
+    Color _color;
 };
