028d7033e0e8b71d4b50ac539416bc7ab2d71b12
Rijenth <a_rijenth@laposte.net>
2025-11-04 12:05:04 +0100

    fix: missing space in files

--- src/progress_bar/ProgressBar.cpp
+++ src/progress_bar/ProgressBar.cpp
@@ -3,7 +3,11 @@
 //
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include "ProgressBar.h"
 =======
 #include "ProgressBar.h"
 >>>>>>> 1d2e8c7 (add: anti aliasing)
+=======
+#include "ProgressBar.h"
+>>>>>>> 913160f (fix: missing space in files)
