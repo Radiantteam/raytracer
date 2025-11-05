018e9776f4c520ccfedd16ccad2e29bd2d68add0
AylineT <ayline.travers@gmail.com>
2025-11-05 14:42:56 +0100

    SCRUM-94 render a scene from a json file

---
+++ src/sceneLoader/SceneLoader.hpp
@@ -0,0 +1,79 @@
+#pragma once
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include "src/Shape.hpp"
+#include "src/sphere/Sphere.hpp"
+#include "src/plane/Plane.hpp"
+#include "src/cube/Cube.hpp"
+#include "src/vec/Vec3.hpp"
+#include "src/color/Color.hpp"
+
+using json = nlohmann::json;
+
+class SceneLoader
+{
+public:
+    struct SceneData
+    {
+        int width;
+        int height;
+        Color background;
+        Vec3 cameraPos;
+        float screenZ;
+        std::vector<std::unique_ptr<Shape>> shapes;
+    };
+
+    static SceneData LoadFromFile(const std::string &filename)
+    {
+        std::ifstream file(filename);
+        if (!file.is_open())
+        {
+            throw std::runtime_error("Cannot open: " + filename);
+        }
+
+        json j;
+        file >> j;
+
+        SceneData scene;
+
+        // Parse image
+        scene.width = j["image"]["width"];
+        scene.height = j["image"]["height"];
+        auto bg = j["image"]["background"];
+        scene.background = Color(bg[0], bg[1], bg[2]);
+
+        // Parse camera
+        auto camPos = j["camera"]["position"];
+        scene.cameraPos = Vec3{camPos[0], camPos[1], camPos[2]};
+        scene.screenZ = j["camera"]["screen_z"];
+
+        // Parse shapes
+        for (const auto &shape : j["shapes"])
+        {
+            std::string type = shape["type"];
+
+            if (type == "sphere")
+            {
+                auto pos = shape["position"];
+                auto col = shape["color"];
+                scene.shapes.push_back(std::make_unique<Sphere>(
+                    Vec3{pos[0], pos[1], pos[2]},
+                    shape["radius"],
+                    Color(col[0], col[1], col[2])));
+            }
+            else if (type == "cube")
+            {
+                auto pos = shape["position"];
+                auto col = shape["color"];
+                scene.shapes.push_back(std::make_unique<Cube>(
+                    Vec3{pos[0], pos[1], pos[2]},
+                    shape["size"],
+                    Color(col[0], col[1], col[2])));
+            }
+        }
+
+        return scene;
+    }
+};
